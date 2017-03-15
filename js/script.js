3b43c19d97a98a3a0b9637f506f7fca3c65a96ec
e-hartung <elizabeth.hartung@gmail.com>
2017-03-14 22:33:39 -0400

    Redesign

---
+++ js/script.js
@@ -0,0 +1,40 @@
+window.sr = ScrollReveal().reveal('.post', {duration: 1000});
+
+
+
+// removing this filtering until portfolio contains more projects (no need for it atm)
+
+// $("a[name='all']").click(function(e) {
+// 	e.preventDefault();
+// 	$(".main div").show();
+// });
+
+// $("a[name='asp-net']").click(function(e) {
+// 	e.preventDefault();
+// 	$(".main div:not(.asp-net)").hide();
+// 	$(".main div.asp-net").show();
+// });
+
+// $("a[name='c-sharp']").click(function(e) {
+// 	e.preventDefault();
+// 	$(".main div:not(.c-sharp)").hide();
+// 	$(".main div.c-sharp").show();
+// });
+
+// $("a[name='front-end']").click(function(e) {
+// 	e.preventDefault();
+// 	$(".main div:not(.front-end)").hide();
+// 	$(".main div.front-end").show();
+// });
+
+// $("a[name='mysql']").click(function(e) {
+// 	e.preventDefault();
+// 	$(".main div:not(.mysql)").hide();
+// 	$(".main div.mysql").show();
+// });
+
+// $("a[name='php']").click(function(e) {
+// 	e.preventDefault();
+// 	$(".main div:not(.php)").hide();
+// 	$(".main div.php").show();
+// });
